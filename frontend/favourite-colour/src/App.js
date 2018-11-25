b206768e61e11eff25425204e752feb97b126a1c
shamanyu <shubham@springboard.com>
2018-11-25 19:21:07 +0530

    Add 404 page

--- frontend/favourite-colour/src/App.js
+++ frontend/favourite-colour/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home-page';
 import ResultsPage from './pages/results-page';
+import Error404Page from './pages/error-404-page';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
             <Route path="/" exact component={HomePage}/>
             <Route path="/results" exact component={ResultsPage}/>
+            <Route path component={Error404Page}/>
         </Switch>
       </BrowserRouter>
     );
---
+++ frontend/favourite-colour/src/pages/error-404-page.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Error404Page extends Component {
+  render() {
+    return (
+      <div>
+          Error 404
+      </div>
+    );
+  }
+}
+
+export default Error404Page;
