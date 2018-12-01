6a13d4c9e40e59406698772c1813da6b2863f5a8
shamanyu <shubham@springboard.com>
2018-12-01 21:16:14 +0530

    Send user vote details on click of homepage

--- frontend/favourite-colour/src/pages/home-page/home-page.js
+++ frontend/favourite-colour/src/pages/home-page/home-page.js
@@ -4,25 +4,44 @@ import { Redirect } from 'react-router';
 import './home-page.css';
 
 class HomePage extends Component {
-  state = {
-    navigate: false
-  }
-
-  render() {
-    const { navigate } = this.state;
-
-    if (navigate) {
-      return <Redirect to="/results" push={true} />
+    state = {
+        navigate: false
     }
 
-    return (
-        <div className="home-page">
-            <div className="options" id="blue-option" onClick={() => this.setState({ navigate: true })}></div>
-            <div className="options" id="yellow-option" onClick={() => this.setState({ navigate: true })}></div>
-            <div className="options" id="red-option" onClick={() => this.setState({ navigate: true })}></div>
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.addVote = this.addVote.bind(this);
+    }
+
+    addVote = (colour) => {
+        fetch('http://localhost:5001/vote', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            colour: colour,
+          })
+        });
+        // this.setState({ navigate: true });
+    }
+
+    render(){
+        const { navigate } = this.state;
+
+        if (navigate) {
+        return <Redirect to="/results" push={true} />
+        }
+
+        return (
+            <div className="home-page">
+                <div className="options" id="blue-option" onClick={this.addVote.bind(this, 'blue')}></div>
+                <div className="options" id="yellow-option" onClick={this.addVote.bind(this, 'yellow')}></div>
+                <div className="options" id="red-option" onClick={this.addVote.bind(this, 'red')}></div>
+            </div>
+        );
+    }
 }
 
 export default HomePage;
