ff06c2eca13710860dae41802d151627816b650c
shamanyu <shubham@springboard.com>
2018-12-01 20:18:10 +0530

    Implement homepage for backend

--- backend/favourite-colour/app/routes.py
+++ backend/favourite-colour/app/routes.py
@@ -1,8 +1,14 @@
-from flask import jsonify
+from flask import jsonify, Response
 
 from app import app
 from .models import ColourVoteCount
 from .serializers import colours_vote_count_schema
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    return Response('APIs for FavouriteColour')
 
 
 @app.route('/results', methods=['GET'])
