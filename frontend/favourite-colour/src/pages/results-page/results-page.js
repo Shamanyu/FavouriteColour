6f6af9869020a3ca5a909893324cf4a559567870
shamanyu <shubham@springboard.com>
2018-12-01 22:15:19 +0530

    Fetch colour to dislay on results page from backend

--- frontend/favourite-colour/src/pages/results-page/results-page.js
+++ frontend/favourite-colour/src/pages/results-page/results-page.js
@@ -1,13 +1,33 @@
 import React, {Component} from "react";
+import $ from 'jquery';
+
+import './results-page.css';
 
 class ResultsPage extends Component {
-  render() {
-    return (
-      <div>
-          Results
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        document.body.style.backgroundColor = this.fetchColour();
+    }
+
+    fetchColour = () => {
+        var colour = "#FFFFFF";
+        $.ajax({
+            async: false,
+            url: "http://localhost:5001/colour",
+            type: 'GET',
+            success: function(res) {
+                colour = res;
+            }
+        });
+        return colour;
+    }
+
+    render() {
+        return(<div></div>);
+    }
+
+
 }
 
 export default ResultsPage;
