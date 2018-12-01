45023c632f3674aa5cbcbcf60a060b25a7df422e
shamanyu <shubham@springboard.com>
2018-12-01 20:46:49 +0530

    Allow all cross-origin requests

--- backend/favourite-colour/app/routes.py
+++ backend/favourite-colour/app/routes.py
@@ -1,4 +1,5 @@
 from flask import jsonify, Response
+from flask_cors import CORS, cross_origin
 
 from app import app
 from .models import ColourVoteCount
@@ -7,12 +8,22 @@ from .serializers import colours_vote_count_schema
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def index():
     return Response('APIs for FavouriteColour')
 
 
 @app.route('/results', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def results():
+    colour_vote_count_data = ColourVoteCount.query.all()
+    result = colours_vote_count_schema.dump(colour_vote_count_data)
+    return jsonify(result.data)
+
+
+@app.route('/vote', methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def vote():
     colour_vote_count_data = ColourVoteCount.query.all()
     result = colours_vote_count_schema.dump(colour_vote_count_data)
     return jsonify(result.data)
