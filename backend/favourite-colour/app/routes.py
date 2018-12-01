6a361529967db11e3fcbbb93f0f636bdba07caaf
shamanyu <shubham@springboard.com>
2018-12-01 09:55:17 +0530

    Bugfix: pep8 adherence

--- backend/favourite-colour/app/routes.py
+++ backend/favourite-colour/app/routes.py
@@ -3,6 +3,7 @@ from flask import Response, jsonify
 from app import app
 from .models import ColourVoteCount
 
+
 @app.route('/results', methods=['GET'])
 def results():
     colour_vote_count_object = ColourVoteCount.query.filter_by(colour='red').first()
